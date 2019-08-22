82928da930e2f8634d6936233399f1e139bbf31f
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-22 08:49:21 +0200

    Add project files.

---
+++ Cirkus/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Cirkus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til cirkus IT-Support");
+            Console.WriteLine("Vi har 4 forskellige typer billetter");
+            Console.WriteLine("Normal: N");
+            Console.WriteLine("Terrasse: T");
+            Console.WriteLine("Parket: P");
+            Console.WriteLine("Loge: L");
+            Console.WriteLine("                 ");
+
+            string valg = Console.ReadLine();
+            int i = 1;
+            // Normal
+            int kapacitet = 100;
+            int pris = 170;
+            //Terrasse
+            int kapacitetT = 150;
+            int prisT = 230;
+            //Parket
+            int kapacitetP = 125;
+            int prisP = 260;
+            //Loge
+            int kapacitetL = 50;
+            int prisL = 350;
+            int totalpris = 0;
+            int antal = 0;
+            switch (valg)
+            {
+                case "n":
+                    while (i <= 2) { 
+                    Console.WriteLine("Hvor mange billetter ønsker du at købe?");
+                    antal = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("                   ");
+                       
+                        
+                    if(i == 1) {
+                            Console.WriteLine("Du har nu købt {0} billetter.", antal);
+                            kapacitet = kapacitet - antal;
+                            Console.WriteLine("Der er nu {0} tilgængelige billetter tilbage.", kapacitet);
+
+                            totalpris = pris * antal;
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din pris er: {0}", totalpris);
+                    }else if (i == 2)
+                        {
+                            totalpris = totalpris + (pris * antal);
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din nye pris er: {0}", totalpris);
+                            Console.WriteLine("Tak for købet af billetter til cirkus IT-Support - Have en god fornøjelse.");
+                        }
+
+                        i++;
+                    }
+                    break;
+                case "t":
+                    while (i <= 2)
+                    {
+                        Console.WriteLine("Hvor mange billetter ønsker du at købe?");
+                        antal = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("                   ");
+                        
+
+                        if (i == 1)
+                        {
+                            Console.WriteLine("Du har nu købt {0} billetter.", antal);
+                            kapacitetT = kapacitetT - antal;
+                            Console.WriteLine("Der er nu {0} tilgængelige billetter tilbage.", kapacitetT);
+                            totalpris = prisT * antal;
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din pris er: {0}", totalpris);
+                        }
+                        else if (i == 2)
+                        {
+                            totalpris = totalpris + (prisT * antal);
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din nye pris er: {0}", totalpris);
+                            Console.WriteLine("Tak for købet af billetter til cirkus IT-Support - Have en god fornøjelse.");
+                        }
+
+                        i++;
+                    }
+                    break;
+
+                case "p":
+                    while (i <= 2)
+                    {
+                        Console.WriteLine("Hvor mange billetter ønsker du at købe?");
+                        antal = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("                   ");
+                        
+
+                        if (i == 1)
+                        {
+                            Console.WriteLine("Du har nu købt {0} billetter.", antal);
+                            kapacitetP = kapacitetP - antal;
+                            Console.WriteLine("Der er nu {0} tilgængelige billetter tilbage.", kapacitetP);
+                            totalpris = prisP * antal;
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din pris er: {0}", totalpris);
+                        }
+                        else if (i == 2)
+                        {
+                            totalpris = totalpris + (prisP * antal);
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din nye pris er: {0}", totalpris);
+                            Console.WriteLine("Tak for købet af billetter til cirkus IT-Support - Have en god fornøjelse.");
+                        }
+
+                        i++;
+                    }
+                    break;
+                case "l":
+                    while (i <= 2)
+                    {
+                        Console.WriteLine("Hvor mange billetter ønsker du at købe?");
+                        antal = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("                   ");
+                        
+
+                        if (i == 1)
+                        {
+                            Console.WriteLine("Du har nu købt {0} billetter.", antal);
+                            kapacitetL = kapacitetL - antal;
+                            Console.WriteLine("Der er nu {0} tilgængelige billetter tilbage.", kapacitetL);
+                            totalpris = prisL * antal;
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din pris er: {0}", totalpris);
+                        }
+                        else if (i == 2)
+                        {
+                            totalpris = totalpris + (prisL * antal);
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("                   ");
+                            Console.WriteLine("Din nye pris er: {0}", totalpris);
+                            Console.WriteLine("Tak for købet af billetter til cirkus IT-Support - Have en god fornøjelse.");
+                        }
+
+                        i++;
+                    }
+                    break;
+            }
+        }
+    }
+}
